c2ce46bf1707f8f5ebb0030bfbadddf037a471ee
Shrirang Nemagoudar <nemagoudarshrirang@gmail.com>
2026-02-12 23:01:50 +0530

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,17 +1,16 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth, signInAnonymously } from 'firebase/auth';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-	appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyCOwQ66go69CWYHqmL2XVr9Sd6gd1n4l4E",
+  authDomain: "study-streak-57383.firebaseapp.com",
+  projectId: "study-streak-57383"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
-signInAnonymously(auth);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const db = firebase.firestore();
+
+export { db, firebase };
